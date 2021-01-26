4c96299f48d9018e4913c7c2dd06050463d8096a
Avinash <avi.gayam@gmail.com>
2021-01-26 13:11:03 -0600

    Unmarshalling a json into user arr, printing value

--- main.go
+++ main.go
@@ -5,10 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-
-	"github.com/gorilla/mux"
+	"strconv"
 )
 
 // Users struct which contains
@@ -33,16 +31,11 @@ type Social struct {
 	Twitter  string `json:"twitter"`
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	//Set content Type and status
-	w.Header().Set("Contetnt-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+func main() {
 	// Open our jsonFile which mimics the data recieved from Mongo
 	jsonFile, err := os.Open("data.json")
-
-	// if we os.Open returns an error then handle it
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 
 	fmt.Println("Successfully Opened users.json")
@@ -62,30 +55,15 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	// we unmarshal our byteArray which contains our jsonFile's content into 'users' which
 	//we defined above. Only those properties will be accepted which are defined in Users struct
 	json.Unmarshal(byteValue, &users)
-	// fmt.Println(users.Users)
-	// usersType := fmt.Sprintf("%T", users.Users)
-	// fmt.Println(usersType)
 
 	// we iterate through every user within our users array and
 	// print out the user Type, their name, and their facebook url
 	// as just an example
-	// for i := 0; i < len(users.Users); i++ {
-	// 	fmt.Println("User Type: " + users.Users[i].Type)
-	// 	fmt.Println("User Age: " + strconv.Itoa(users.Users[i].Age))
-	// 	fmt.Println("User Name: " + users.Users[i].Name)
-	// 	fmt.Println("Facebook Url: " + users.Users[i].Social.Facebook)
-	// }
-	//we marshal back the json content based on the Users struct defination.
-	resp, err := json.Marshal(users.Users)
-	if err != nil {
-		log.Fatal(err)
+	for i := 0; i < len(users.Users); i++ {
+		fmt.Println("User Type: " + users.Users[i].Type)
+		fmt.Println("User Age: " + strconv.Itoa(users.Users[i].Age))
+		fmt.Println("User Name: " + users.Users[i].Name)
+		fmt.Println("Facebook Url: " + users.Users[i].Social.Facebook)
 	}
-	w.Write(resp)
 
-}
-
-func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", HomeHandler).Methods(http.MethodGet)
-	http.ListenAndServe(":8080", r)
 }
